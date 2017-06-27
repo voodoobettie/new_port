049ea699bb63aaa5f37d5373f60845d90c2cd899
Ingrid Molloy <ingrid.molloy@shopify.com>
2017-06-27 14:33:44 -0700

    added pages and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  resources :portfolios
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
